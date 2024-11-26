b6cd79f9d9fd23f1815fe3b87982f5e74c722627
santosh gusain <santoshgusain@pop-os.localdomain>
2024-11-26 16:46:58 +0530

    made change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,23 @@ export default function HomePage() {
   return (
     <main>
       <section className="py-16 md:py-24 lg:py-32">
-        This is the customised Spliit.
+      <div className="container flex max-w-screen-md flex-col items-center gap-4 text-center">
+          <h1 className="!leading-none font-bold text-2xl sm:text-3xl md:text-4xl lg:text-5xl landing-header py-2">
+            {t.rich('Homepage.title', {
+              strong: (chunks) => <strong>{chunks}</strong>,
+            })}
+          </h1>
+          <p className="max-w-[42rem] leading-normal text-muted-foreground sm:text-xl sm:leading-8">
+            {t.rich('Homepage.description', {
+              strong: (chunks) => <strong>{chunks}</strong>,
+            })}
+          </p>
+          <div className="flex gap-2">
+            <Button asChild>
+              <Link href="/groups">{t('Homepage.button.groups')}</Link>
+            </Button>
+          </div>
+        </div>
       </section>
     </main>
   )
